87d45c81c7325ae48038432cb365a6266dcf449b
Mursheda Baby <murshedamaya@gmail.com>
2024-07-24 17:51:07 +0200

    modified new

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,8 +55,23 @@ resource "aws_ecs_service" "main" {
   }
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  # owners = ["099720109477"] # Canonical
+}
 resource "aws_instance" "ecs_instance" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   # subnet_id     = data.aws_subnets.default.ids
   for_each      = toset(data.aws_subnets.default.ids)
